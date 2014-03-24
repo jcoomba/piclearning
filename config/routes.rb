dd5ba4cdcdb222ff324cfc0db23153c7e8dab3b1
jcoomba <jason@meuspics.com>
2014-03-24 15:08:21 -0300

    added about and how pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 PIClearning::Application.routes.draw do
+  root "pages#home"
+  get "about" => 'pages#about'
+  get "how" => 'pages#how'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
