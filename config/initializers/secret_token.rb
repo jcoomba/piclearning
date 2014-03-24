81a239b71e1a73148f7432968ec4584730ca78f2
jcoomba <jason@meuspics.com>
2014-03-24 14:01:10 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PIClearning::Application.config.secret_key_base = '4fb8955433f39a18d491ff92b827bcbae14b91095a4b6113201dd0a881cfa282deb67640c70e8ed3e1dbb4980fc8e02825c1f36157b5d51950543c3a5c494ad9'
